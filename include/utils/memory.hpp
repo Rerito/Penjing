ba797cec17ded6a3fc8cc9b0fe4aa6c55fc224e6
Remi Marty <4198746+Rerito@users.noreply.github.com>
2019-01-13 19:34:33 +0100

    Added a convenience feature to handle unique_ptr with custom allocators

---
+++ include/utils/memory.hpp
@@ -0,0 +1,37 @@
+
+#pragma once
+
+#include <memory>
+#include "utils/forward_macro.hpp"
+
+namespace memory {
+
+struct no_destruction {
+    template <typename T>
+    void destroy(T*) const noexcept {}
+};
+
+struct with_destruction {
+    template <typename T>
+    void destroy(T* p) const noexcept {
+        std::destroy(p, p+1);
+    }
+};
+
+template <typename Alloc, typename DestroyPolicy = with_destruction>
+struct custom_alloc_deleter : DestroyPolicy {
+    template <typename T>
+    void operator()(T *p) const {
+        DestroyPolicy::destroy(p);
+        Alloc{}.deallocate(p, 1);
+    }
+};
+
+template <typename T, typename Alloc, typename... Args>
+auto make_unique(Args&&... args) {
+    auto ptrStorage = std::unique_ptr<T, custom_alloc_deleter<Alloc, no_destruction> > (Alloc{}.allocate(1));
+    new (ptrStorage.get()) T(CPPDWD(args)...);
+    return std::unique_ptr<T, custom_alloc_deleter<Alloc, with_destruction> >(ptrStorage.release());
+}
+
+} // namespace memory
---
+++ tests/test_memory.cpp
@@ -0,0 +1,52 @@
+
+#include "gtest/gtest.h"
+#include "utils/memory.hpp"
+
+struct life_tracker {
+    static size_t nb_construct;
+    static size_t nb_destruct;
+
+    life_tracker() { ++nb_construct; }
+    ~life_tracker() { ++nb_destruct; }
+};
+
+size_t life_tracker::nb_construct = 0u;
+size_t life_tracker::nb_destruct = 0u;
+
+template <typename T>
+struct test_allocator : std::allocator<T> {
+    static size_t nb_alloc;
+    static size_t nb_dealloc;
+
+    void deallocate(T *p, size_t n) {
+        nb_dealloc += n;
+        std::allocator<T>::deallocate(p, n);
+    }
+
+    T* allocate(size_t n) {
+        auto ptr = std::allocator<T>::allocate(n);
+        nb_alloc += n;
+        return ptr;
+    }
+        
+};
+
+template <typename T>
+size_t test_allocator<T>::nb_alloc = 0;
+
+template <typename T>
+size_t test_allocator<T>::nb_dealloc = 0;
+
+struct MemoryTest : testing::Test {
+};
+
+TEST_F(MemoryTest, TestMakeUnique) {
+    using memory::make_unique;
+    {
+        auto ptr = make_unique<life_tracker, test_allocator<life_tracker> >();
+    }
+    ASSERT_EQ(test_allocator<life_tracker>::nb_alloc, test_allocator<life_tracker>::nb_dealloc);
+    ASSERT_EQ(test_allocator<life_tracker>::nb_alloc, 1u);
+    ASSERT_EQ(life_tracker::nb_construct, life_tracker::nb_destruct);
+    ASSERT_EQ(life_tracker::nb_construct, 1u);
+}
