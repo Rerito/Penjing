9535a5cd6e9f35f434be58e0a8e97efb76d0542c
Remi Marty <4198746+Rerito@users.noreply.github.com>
2021-02-07 12:27:02 +0100

    meta: Add a meta interface library dedicated to TMP blocks

---
+++ meta/include/Meta/IsDetected.hpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2021, Rerito
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <type_traits>
+
+namespace Meta {
+
+namespace Details {
+
+template< template< typename... > class, typename, typename... >
+struct IsDetected : std::false_type
+{};
+
+template< template< typename... > class Z, typename... Args >
+struct IsDetected< Z, std::void_t< Z< Args... > >, Args... > : std::true_type
+{};
+
+} // namespace Details
+
+// Detection idiom for the given template Z and arguments Args...
+// Member value evaluates to true if `Z< Args... >` is a valid specialization
+// of `Z`.
+template< template< typename... > class Z, typename... Args >
+using IsDetected = Details::IsDetected< Z, void, Args... >;
+
+// IsDetected as a concept.
+template< template< typename... > class Z, typename... Args >
+concept Detected = IsDetected< Z, Args... >::value;
+
+} // namespace Meta
