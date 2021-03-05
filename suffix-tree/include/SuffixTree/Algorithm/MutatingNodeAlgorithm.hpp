3aeaf7968bee65a8aa7415c53419fc7fbd3194f0
Remi Marty <4198746+Rerito@users.noreply.github.com>
2021-03-05 22:58:54 +0100

    suffix-tree/Algorithm: Add MutatingNodeAlgorithm base class

---
+++ suffix-tree/include/SuffixTree/Algorithm/MutatingNodeAlgorithm.hpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2021, Rerito
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <utility>
+
+#include "../Core/UnsafeTag.hpp"
+
+namespace SuffixTree {
+namespace Algorithm {
+
+class MutatingNodeAlgorithm
+{
+protected:
+    template< typename Node >
+    constexpr auto mutableTransition(Node& node, typename Node::CharType start)
+        const noexcept(noexcept(node.mutableTransition({}, std::move(start))))
+    {
+        return node.mutableTransition({}, std::move(start));
+    }
+
+    template< typename Node >
+    constexpr auto& mutableTransitionUnsafe(
+        Node& node,
+        typename Node::CharType start) const
+        noexcept(noexcept(
+            node.mutableTransition({}, std::move(start), Core::UnsafeTag{})))
+    {
+        return node.mutableTransition({}, std::move(start), Core::UnsafeTag{});
+    }
+
+    template< typename Node >
+    constexpr void setLink(Node& node, typename Node::NodePtr link) const
+        noexcept(noexcept(node.setLink({}, std::move(link))))
+    {
+        node.setLink({}, std::move(link));
+    }
+
+    template< typename Node >
+    constexpr auto addTransition(
+        Node& node,
+        typename Node::StringViewType label,
+        typename Node::NodePtr target) const
+        noexcept(noexcept(
+            node.addTransition({}, std::move(label), std::move(target))))
+    {
+        return node.addTransition({}, std::move(label), std::move(target));
+    }
+};
+
+} // namespace Algorithm
+} // namespace SuffixTree
