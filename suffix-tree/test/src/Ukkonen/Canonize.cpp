c2e1bd0077db73a71276fbd0d3bdfb0256cac461
Remi Marty <4198746+Rerito@users.noreply.github.com>
2021-03-06 22:56:54 +0100

    suffix-tree/test/Ukkonen: Add Canonize CPO unit tests

---
+++ suffix-tree/test/src/Ukkonen/Canonize.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2021, Rerito
+// SPDX-License-Identifier: MIT
+
+#include <gtest/gtest.h>
+
+#include <SuffixTree/Builders/Ukkonen/Canonize.hpp>
+
+#include <BananaFixture.hpp>
+
+using namespace std::string_literals;
+
+using UkkonenCanonizeFixture = SuffixTree::Test::BananaFixture;
+
+TEST_F(UkkonenCanonizeFixture, Explicit)
+{
+    std::string ana = "ana"s;
+    auto [node, path] = SuffixTree::Builders::Ukkonen::canonize(
+        n_root,
+        std::string_view{ana});
+
+    ASSERT_TRUE(std::ranges::empty(path));
+    ASSERT_EQ(&node, &n_ana);
+}
+
+TEST_F(UkkonenCanonizeFixture, Implicit)
+{
+    std::string anan = "anan"s;
+
+    std::string n = "n"s;
+
+    auto [node, path] = SuffixTree::Builders::Ukkonen::canonize(
+        n_root,
+        std::string_view{anan});
+
+    ASSERT_EQ(path, n);
+    ASSERT_EQ(&node, &n_ana);
+}
+
+TEST_F(UkkonenCanonizeFixture, Deepest)
+{
+    std::string anana_ = "anana$"s;
+
+    auto [node, path] = SuffixTree::Builders::Ukkonen::canonize(
+        n_root,
+        std::string_view{anana_});
+
+    ASSERT_TRUE(std::ranges::empty(path));
+    ASSERT_EQ(&node, &n_anana_);
+}
