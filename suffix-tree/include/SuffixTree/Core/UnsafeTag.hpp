89c7fbc0c7b2048487fb37acad8e952d36b6e57d
Remi Marty <4198746+Rerito@users.noreply.github.com>
2021-03-11 22:32:58 +0100

    suffix-tree/Core: Add UnsafeTag class to tag dispatch unsafe accessors

---
+++ suffix-tree/include/SuffixTree/Core/UnsafeTag.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2021, Rerito
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace SuffixTree {
+namespace Core {
+
+struct UnsafeTag
+{
+    constexpr explicit UnsafeTag() noexcept = default;
+};
+
+} // namespace Core
+} // namesapce SuffixTree
