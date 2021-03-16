1e0fd5f7dabe09d068ad0d38df3bd281f302ad12
Remi Marty <4198746+Rerito@users.noreply.github.com>
2021-03-16 16:37:06 +0100

    suffix-tree/test: Add BuildString test case

---
+++ suffix-tree/test/include/Dump.hpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2021, Rerito
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include <SuffixTree/Concepts/Node.hpp>
+
+namespace SuffixTree {
+namespace Test {
+
+class Dump
+{
+
+    template< typename Node >
+        requires(Concepts::Node< Node >)
+    constexpr void dump(Node const& node, std::ostream& ostream, size_t offset)
+        const
+    {
+        std::string whitespace(offset, ' ');
+        auto transitions = node.transitions();
+        for (auto t : transitions) {
+            ostream << whitespace << "\\--[" << t.get().label() << "]-->o"
+                    << std::endl;
+            dump(
+                *(t.get()),
+                ostream,
+                offset + 8 + std::ranges::size(t.get().label()));
+        }
+    }
+
+public:
+    template< typename Node >
+    constexpr void operator()(Node const& node, std::ostream& ostream) const
+    {
+        ostream << 'o' << std::endl;
+        dump(node, ostream, 0u);
+    }
+};
+
+inline namespace Cust {
+
+inline constexpr Dump dump{};
+
+} // namespace Cust
+
+} // namespace Test
+} // namespace SuffixTree
---
+++ suffix-tree/test/src/Ukkonen/BuildString.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) 2021, Rerito
+// SPDX-License-Identifier: MIT
+
+#include <gtest/gtest.h>
+
+#include <SuffixTree/Algorithm/CompareNodes.hpp>
+
+#include <SuffixTree/Builders/Ukkonen/BuildString.hpp>
+#include <SuffixTree/Builders/Ukkonen/Canonize.hpp>
+#include <SuffixTree/Builders/Ukkonen/Split.hpp>
+#include <SuffixTree/Builders/Ukkonen/TestAndSplit.hpp>
+#include <SuffixTree/Builders/Ukkonen/Update.hpp>
+
+#include <BananaFixture.hpp>
+#include <NodeFactory.hpp>
+#include <Dump.hpp>
+
+using namespace SuffixTree::Builders::Ukkonen;
+using namespace SuffixTree::Test;
+
+using UkkonenBuildStringFixture = BananaFixture;
+
+inline constexpr CPO::BuildString<
+    CanonizePolicy,
+    UpdatePolicy< CanonizePolicy, TestAndSplitPolicy< SplitPolicy > > >
+    buildString{};
+
+TEST_F(UkkonenBuildStringFixture, Mississipi)
+{
+    NodeFactory< NodeType > factory{};
+    auto& root = factory();
+    std::string mississipi = "mississipi$";
+    ::buildString(root, mississipi, factory);
+    dump(root, std::cout);
+}
+
+TEST_F(UkkonenBuildStringFixture, Build)
+{
+    NodeFactory< NodeType > factory{};
+
+    auto& root = factory();
+    ::buildString(root, _banana, factory);
+    EXPECT_TRUE(SuffixTree::Algorithm::compareNodes(root, _nodes.at(0)));
+
+    std::string cacao = "cacao$";
+    ::buildString(root, cacao, factory);
+
+    std::string curacao = "curacao$";
+    ::buildString(root, curacao, factory);
+
+    std::string burundi = "burundi$";
+    ::buildString(root, burundi, factory);
+    dump(root, std::cout);
+}
