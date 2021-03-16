611112b7728d92606c9cdcf7785210eb7cf0f95c
Remi Marty <4198746+Rerito@users.noreply.github.com>
2021-03-16 16:35:47 +0100

    suffix-tree/Builders/Ukkonen: Add BuildString CPO

---
+++ suffix-tree/include/SuffixTree/Builders/Ukkonen/BuildString.hpp
@@ -0,0 +1,105 @@
+// Copyright (c) 2021, Rerito
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "../../Algorithm/MutatingNodeAlgorithm.hpp"
+#include "../../Algorithm/Walk.hpp"
+
+namespace SuffixTree {
+namespace Builders {
+namespace Ukkonen {
+
+namespace CPO {
+
+template< typename Canonizer, typename Updater >
+class BuildString
+    : private Algorithm::MutatingNodeAlgorithm
+    , private Canonizer
+    , private Updater
+{
+private:
+    using Canonizer::canonize;
+    using Updater::update;
+
+private:
+    // TODO: Write proper noexcept specifier
+    template< typename Node, typename NodeFactory >
+    static constexpr bool _isNoExcept()
+    {
+        return false;
+    }
+
+public:
+    template< typename Node, typename NodeFactory >
+    constexpr void operator()(
+        Node& root,
+        typename Node::StringViewType word,
+        NodeFactory&& makeNode) const
+        noexcept(_isNoExcept< Node, NodeFactory&& >())
+    {
+        using StrView = typename Node::StringViewType;
+        using DiffType = std::ranges::range_difference_t< StrView >;
+
+        auto [initialActiveNode, toInsert, currentExpansion] =
+            fetchActivePoint(root, word);
+
+        auto activeNode = std::ref(initialActiveNode);
+
+        while (std::ranges::size(toInsert) > currentExpansion) {
+            StrView label{};
+
+            // label is the proper view on the string for the next insertion
+            // However it still needs to be canonized
+            std::tie(activeNode, label) = update(
+                root,
+                activeNode.get(),
+                toInsert,
+                currentExpansion,
+                makeNode);
+
+            auto [canonizedActiveNode, canonizedLabel] =
+                canonize(activeNode.get(), label);
+            label = std::move(canonizedLabel);
+
+            // To be able to properly perform the next insertion, we have to
+            // suffix the label view with the remainder of the toInsert view.
+            // This is possible because the implementation guarantees that label
+            // and toInsert are contiguous views over the same string.
+            toInsert = {std::ranges::begin(label), std::ranges::end(toInsert)};
+
+            // The current expansion for the next update call is marked by the
+            // end of the label view.
+            currentExpansion = std::ranges::size(label);
+            activeNode = std::ref(const_cast< Node& >(canonizedActiveNode));
+        }
+    }
+
+private:
+    template< typename Node >
+    constexpr auto fetchActivePoint(
+        Node& root,
+        typename Node::StringViewType word) const
+    {
+        // Fetch the active point
+        auto [activeNode, toInsert, expansion] = Algorithm::walk(root, word);
+
+        return std::make_tuple(
+            std::ref(const_cast< Node& >(activeNode)),
+            std::move(toInsert),
+            std::move(expansion));
+    }
+};
+
+} // namespace CPO
+
+inline namespace Cust {
+
+template< typename Canonizer, typename Updater >
+inline constexpr CPO::BuildString< Canonizer, Updater > buildString{};
+
+} // namespace Cust
+
+} // namespace Ukkonen
+} // namespace Builders
+} // namespace SuffixTree
